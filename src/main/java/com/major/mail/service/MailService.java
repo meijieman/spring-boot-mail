45e9fa8fbcd9b439e1060ece10bd1ae3842060d2
meijieman <1558667079@qq.com>
2018-09-07 22:50:44 +0800

    添加实例

--- src/main/java/com/major/mail/service/MailService.java
+++ src/main/java/com/major/mail/service/MailService.java
@@ -4,10 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+/**
+ * 参考慕课网 《Spring Boot 发送邮件》
+ */
 @Service
 public class MailService {
 
@@ -19,23 +28,66 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void say(){
+    public void say() {
         log.debug("hello!");
         log.info("hello!");
         log.warn("hello!");
         log.error("hello!");
     }
 
-    public void sendMail(String to, String subject, String content){
+    public void sendMail(String to, String subject, String content) {
+        log.info("发送邮件{},{},{}", to, subject, content);
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setFrom(from);
         message.setText(content);
+//        message.setReplyTo(FROM_MAIL); // 回复邮箱
+//        message.setCc(CC_MAIL); // 抄送邮箱
+//        message.setBcc(BCC_MAIl); // 密送邮箱
+//        message.setSentDate(date); // 发送日期
+
         mailSender.send(message);
     }
 
-    public void sendHtmlMail(String to, String subject, String content) {
+    public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
 
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setFrom(from);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        mailSender.send(message);
+    }
+
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String filename = file.getFilename();
+        helper.addAttachment(filename, file);
+        // 可以添加多个附件
+        mailSender.send(message);
+    }
+
+    // 带图片的邮件
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+
+        FileSystemResource res = new FileSystemResource(new File(rscPath));
+        helper.addInline(rscId, res);
+        // 可以添加多个邮件正文图片资源
+        mailSender.send(message);
     }
 }
--- src/test/java/com/major/mail/service/MailServiceTest.java
+++ src/test/java/com/major/mail/service/MailServiceTest.java
@@ -4,18 +4,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
-
-import static org.junit.Assert.*;
+import javax.mail.MessagingException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MailServiceTest {
 
 
+    private static final String TO = "jie.mei@lunzn.com";
+
     @Resource
     private MailService mailService;
+
+    @Resource
+    private TemplateEngine templateEngine;
 
     @Test
     public void sayTest() {
@@ -24,7 +30,43 @@ public class MailServiceTest {
 
     @Test
     public void sendMailTest() {
-        mailService.sendMail("jie.mei@lunzn.com", "这是第一封邮件主题", "这是第一封邮件内容");
+        mailService.sendMail(TO, "这是第一封邮件主题", "这是第一封邮件内容");
     }
 
+    @Test
+    public void sendHtmlMailTest() throws MessagingException {
+        String content = "<html><body>" +
+                "<h3>hello world, 这是一封 html 邮件</h3>" +
+                "</body></html>";
+        mailService.sendHtmlMail(TO, "这是html邮件主题", content);
+    }
+
+    @Test
+    public void sendAttachmentsMailTest() throws MessagingException {
+        String content = "<html><body>" +
+                "邮件正文" +
+                "</body></html>";
+        String filePath = "./file/Chat.rar";
+        mailService.sendAttachmentsMail(TO, "这是包含附件的邮件主题", content, filePath);
+    }
+
+    @Test
+    public void sendInlineResourceMailTest() throws MessagingException {
+        String imgPath = "./file/404.jpg";
+        String rscId = "ha001";
+        String content = "<html><body>这是有图片的邮件：" +
+                "<img src=\'cid:" + rscId + "\'></img>" +
+                "</body></html>";
+
+        mailService.sendInlineResourceMail(TO, "这是一个带有图片的邮件主题", content, imgPath, rscId);
+    }
+
+    @Test
+    public void testTemplateMailTest() throws MessagingException {
+        Context context = new Context();
+        context.setVariable("id", "006");
+
+        String emailContet = templateEngine.process("emailTemplate", context);
+        mailService.sendHtmlMail(TO, "这是一个模版邮件", emailContet);
+    }
 }
